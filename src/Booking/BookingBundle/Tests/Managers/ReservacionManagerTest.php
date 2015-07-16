6aaa0e211d29a4af1ccaa814ee0119cde219985e
aiorian <firodriguez@emailn.de>
2015-07-15 23:03:30 -0400

    ADDED: Temporal commit

---
+++ src/Booking/BookingBundle/Tests/Managers/ReservacionManagerTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: firomero
+ * Date: 11/07/15
+ * Time: 9:17
+ */
+
+namespace Booking\BookingBundle\Tests\Managers;
+
+
+class ReservacionManagerTest extends \PHPUnit_Framework_TestCase {
+
+    public function testCasa(){
+        $casa = [
+            4,3,2,1,
+
+        ];
+
+        $quest = [
+            2,2
+        ];
+
+
+    }
+
+}
+ 
